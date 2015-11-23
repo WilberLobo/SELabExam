a278b2a8a75465a1af9a2c9f31e9cb32029ed3fe
WilberLobo <lobowilber07@gmail.com>
2015-11-23 16:47:37 +0530

    code commit validation

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,4 +1,5 @@
 class Requirement < ActiveRecord::Base
 	validates :name, :description, :contact, presence: true
 	validates :contact, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+	validates :name, uniqueness: true
 end
